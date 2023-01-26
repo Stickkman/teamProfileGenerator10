8a89dcddabff32d9c2caf8824618e4ae9a97e872
stickkman <stickkman@gmail.com>
2023-01-25 20:28:03 -0500

    finshed code for all prompts and their ordering

--- Main/index.js
+++ Main/index.js
@@ -160,18 +160,80 @@ const promptEngineer = ()=> {
         //instantiate new engineer object with required properties from input answers
         const engineer = new Engineer(answers.name, answers.idNumber, answers.email, answers.githubUsername);
         teamMembers.push(engineer);  // push new engineer object into teamMembers array
-            promptMenu();           // calls promptMenu function to add additional employees or build team now.
+            promptMenu();           // go back to menu
     })
 };
-
-
-
-
-
-if (!fs.existsSync(DIST_DIR)) {
+// function for adding Intern to team
+const promptIntern = ()=> { 
+    return inquirer.prompt([  
+    {
+        type: 'input',                            
+        message: "Please enter Intern's name? ",  
+        name: 'name',                             
+        validate: (name)=> {                      
+            if (name !=='') { 
+                return true;
+            } else {
+                console.log('Please enter at least one character..'); // catch for wrong input type
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: "Intern Employee Id Number? ",
+        name: 'idNumber',
+        validate: (idNumber)=> { 
+            if(idNumber !=='') {
+                return true;
+            } else { 
+                console.log('Please enter at least one NUMBER..');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: "Intern Email? ",
+        name: 'email',
+        validate: (email)=> { 
+            if(email !=='') {
+                return true;
+            } else { 
+                console.log('No input entered, please try again..');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: "Intern School? ",
+        name: 'school',
+        validate: (school)=> { 
+            if(school !=='') {
+                return true;
+            } else { 
+                console.log('No school has been entered, please try again..');
+                return false;
+            }
+        }
+    },
+]).then(answers => {
+    console.log("Testing answers" + answers); //REMOVE THIS LATER
+    //instantiate new intern object with required properties from input answers
+    const intern = new Intern(answers.name, answers.idNumber, answers.email, answers.school);
+    teamMembers.push(engineer);  // push new intern object into teamMembers array
+        promptMenu();           // go back to menu
+})
+};
+// function to build team
+const buildTeam = ()=> {
+    if (!fs.existsSync(DIST_DIR)) { 
      // if 'dist' directory does not exist create it
-    fs.mkdirSync(DIST_DIR) 
+        fs.mkdirSync(DIST_DIR) 
+    }
+    //write file instantly to designated path by calling generatePage function and passing teamMembers array of objects
+    // fs.writeFileSync(distPath, generatePage(teamMembers), 'utf-8');
 }
-    //write file
-fs.writeFileSync(distPath, generatePage(teamMembers), 'utf-8');
 
+promptManager(); // call to start prompts, beginning with manager prompts
